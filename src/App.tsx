cddd7112b5baab67dcd711406ac3ee84538a6aec
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-19 17:52:37 -0400

    refactor: Optimize movie list rendering and add hover effects

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from 'react'
 
 export const App = () => {
 	const [moviesData, setMoviesData] = useState<MovieType[]>([])
-	useEffect(() => {
+
+	const getAllMovies = () => {
 		try {
 			fetch('http://localhost:3000/movies/', {
 				method: 'GET',
@@ -13,7 +14,7 @@ export const App = () => {
 		} catch (error) {
 			console.error(error)
 		}
-	}, [])
+	}
 
 	const deleteMovie = async (id: number) => {
 		try {
@@ -22,36 +23,49 @@ export const App = () => {
 			})
 				.then((response) => response.json())
 				.then((data) => console.log(data))
-        if (moviesData) {
-          const newMoviesData = moviesData.filter((movie) => movie.id !== id)
-          setMoviesData(newMoviesData)
-        }
+			if (moviesData) {
+				const newMoviesData = moviesData.filter(
+					(movie) => movie.id !== id
+				)
+				setMoviesData(newMoviesData)
+			}
 		} catch (error) {
 			console.error(error)
 		}
 	}
+
+	useEffect(() => {
+		getAllMovies()
+	}, [])
 	return (
-		<>
-			<h1>Movie List</h1>
+		<div className="px-12">
+			<h1 className="font-bold my-12 mx-3">Movie List</h1>
 			<ul>
-				<li className="grid grid-cols-5 grid-flow-row gap-8">
+				<li className="grid grid-cols-6 grid-flow-row gap-8">
 					{moviesData.map((movie) => {
 						return (
 							<div
 								key={movie.id}
-								className="text-lg font-semibold text-center"
+								className="text-lg font-semibold text-center hover:scale-110 transition-all duration-200 ease-in-out"
 							>
 								<div className="group/movie relative">
-									<img
-										src={movie.poster}
-										alt={movie.title}
-										className="group-hover/movie:opacity-30 transition duration-150 ease-in-out h-96 w-full object-cover"
-									/>
-									<div className="absolute invisible group-hover/movie:visible top-0 w-full h-full ">
-										<h2>{movie.title}</h2>
-										<p>
-											{movie.year} <span>IMDB: {movie.rate}</span>
+									<picture>
+										<img
+											src={movie.poster}
+											alt={movie.title}
+											className="group-hover/movie:opacity-10  h-96 w-full object-cover group-hover/movie:rounded-lg"
+										/>
+									</picture>
+									<div className="absolute invisible group-hover/movie:visible top-0 w-full h-full flex flex-col p-3 text-start">
+										<h3 className="mb-3 text-base	">{movie.title}</h3>
+										<p className="text-sm">
+											<span>★</span> {movie.rate}
 										</p>
+										<h4 className="text-sm text-slate-300">
+											{movie.description}
+										</h4>
+										<div></div>
+										<p>{movie.year}</p>
 									</div>
 								</div>
 								<h2>{movie.title}</h2>
@@ -63,10 +77,9 @@ export const App = () => {
 					})}
 				</li>
 			</ul>
-		</>
+		</div>
 	)
 }
-
 interface MovieType {
 	id: number
 	title: string
@@ -74,4 +87,6 @@ interface MovieType {
 	genre: string
 	rate: number
 	poster: string
+	description: string
+	duration: number
 }
