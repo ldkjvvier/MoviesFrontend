5d14595bc5f60fa7e0162d2240c86f24cbd97690
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-20 02:34:10 -0400

    feat: Add new icons for Home, Movie, Search, and Serie components

---
+++ src/components/icons/Serie.tsx
@@ -0,0 +1,13 @@
+export const SerieIcon = () => {
+  return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+      className="fill-current text-white w-6 h-6"
+		>
+			<path d="M20 6h-5.586l2.293-2.293-1.414-1.414L12 5.586 8.707 2.293 7.293 3.707 9.586 6H4c-1.103 0-2 .897-2 2v11c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V8c0-1.103-.897-2-2-2z"></path>
+		</svg>
+	)
+}
