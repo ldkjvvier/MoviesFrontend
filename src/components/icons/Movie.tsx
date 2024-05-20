6703d0f48f327f0ff4ab4db52ab67fd812a43d1b
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-20 02:52:05 -0400

    feat: Update icons to accept size prop for dynamic sizing

--- src/components/icons/Movie.tsx
+++ src/components/icons/Movie.tsx
@@ -1,11 +1,12 @@
-export const MovieIcon = () => {
-  return (
+export const MovieIcon = ({ size = 6 }: { size?: number }) => {
+	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="24"
 			height="24"
+			role="img"
 			viewBox="0 0 24 24"
-      className="fill-current text-white w-6 h-6"
+			className={`fill-current text-white w-${size} h-${size}`}
 		>
 			<path d="M20 3H4c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V5c0-1.103-.897-2-2-2zm.001 6c-.001 0-.001 0 0 0h-.466l-2.667-4H20l.001 4zm-5.466 0-2.667-4h2.596l2.667 4h-2.596zm-2.404 0H9.535L6.869 5h2.596l2.666 4zM4 5h.465l2.667 4H4V5z"></path>
 		</svg>
--- src/components/icons/Serie.tsx
+++ src/components/icons/Serie.tsx
@@ -1,13 +1,14 @@
-export const SerieIcon = () => {
-  return (
+export const SerieIcon = ({ size = 6 }: { size?: number }) => {
+	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
 			width="24"
 			height="24"
+			role="img"
 			viewBox="0 0 24 24"
-      className="fill-current text-white w-6 h-6"
+			className={`fill-current text-white w-${size} h-${size}`}
 		>
-			<path d="M20 6h-5.586l2.293-2.293-1.414-1.414L12 5.586 8.707 2.293 7.293 3.707 9.586 6H4c-1.103 0-2 .897-2 2v11c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V8c0-1.103-.897-2-2-2z"></path>
+			<path d="M20 6h-5.586l2.293-2.293-1.414-1.414L12 5.586 8.707 2.293 7.293 3.707 9.586 6H4c-1.103 0-2 .897-2 2v11c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V8c0-1.103-.897-2-2-2zM4 19V8h16l.002 11H4z"></path>
 		</svg>
 	)
 }
