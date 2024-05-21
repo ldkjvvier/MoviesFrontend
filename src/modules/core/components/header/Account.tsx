28732a77ebf121b857761a50a959e707ee8df933
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-20 21:13:04 -0400

    feat: Add Account component to header

---
+++ src/modules/core/components/header/Account.tsx
@@ -0,0 +1,13 @@
+export const Account = () => {
+	return (
+		<div className="flex items-center gap-3">
+			Username
+			{/* User Icon */}
+			<img
+				src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+				alt=""
+        className="w-8 h-8"
+			/>
+		</div>
+	)
+}
