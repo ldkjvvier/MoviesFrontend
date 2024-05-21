c940922081a0ebd35f8f2a9d4dae581f0d9c0519
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-20 21:32:53 -0400

    refactor: Update header navigation component and routes

--- src/modules/core/components/header/Header.tsx
+++ src/modules/core/components/header/Header.tsx
@@ -1,37 +1,14 @@
-import { Link } from 'react-router-dom'
-import { HomeIcon } from '../../../../components/icons/Home'
-import { MovieIcon } from '../../../../components/icons/Movie'
-import { SearchIcon } from '../../../../components/icons/Search'
-import { SerieIcon } from '../../../../components/icons/Serie'
+import { HeaderNav } from './HeaderNav'
 import { Account } from './Account'
-const routes = [
-	{ path: '/',       name: 'Home',   icon: <HomeIcon size={5} /> },
-	{ path: '/search', name: 'search', icon: <SearchIcon size={5} /> },
-	{ path: '/movies', name: 'Movies', icon: <MovieIcon size={5} /> },
-	{ path: '/series', name: 'Series', icon: <SerieIcon size={5} /> },
-]
+
 export const Header = () => {
 	return (
 		<header>
-			<nav className="flex bg-black px-3 md:px-12 py-4 justify-between">
+			<nav className="flex bg-secondary px-3 md:px-12 py-4 justify-between">
 				<h1 className="text-white text-2xl text-center">
 					Movies
 				</h1>
-				<div className="flex items-center">
-					<ul className="grid grid-flow-col gap-3 md:gap-12">
-						{routes.map((route) => (
-							<li key={route.path}>
-								<Link
-									to={route.path}
-									className="flex items-center gap-2 text-white hover:text-gray-300 text-lg"
-								>
-									<span>{route.icon}</span>
-									<span className='hidden md:inline'>{route.name}</span>
-								</Link>
-							</li>
-						))}
-					</ul>
-				</div>
+				<HeaderNav />
 				<Account />
 			</nav>
 		</header>
---
+++ src/modules/core/components/header/HeaderNav.tsx
@@ -0,0 +1,31 @@
+import { Link } from 'react-router-dom'
+import { HomeIcon } from '../../../../components/icons/Home'
+import { MovieIcon } from '../../../../components/icons/Movie'
+import { SearchIcon } from '../../../../components/icons/Search'
+import { SerieIcon } from '../../../../components/icons/Serie'
+
+export const HeaderNav = () => {
+	const routes = [
+		{ path: '/',       name: 'Home',   icon: <HomeIcon   size={5} /> },
+		{ path: '/search', name: 'search', icon: <SearchIcon size={5} />,},
+		{ path: '/movies', name: 'Movies', icon: <MovieIcon  size={5} /> },
+		{ path: '/series', name: 'Series', icon: <SerieIcon  size={5} /> },
+	]
+	return (
+		<div className="flex items-center">
+			<ul className="grid grid-flow-col gap-3 md:gap-12">
+				{routes.map((route) => (
+					<li key={route.path}>
+						<Link
+							to={route.path}
+							className="flex items-center gap-2 text-white hover:text-gray-300 text-lg"
+						>
+							<span>{route.icon}</span>
+							<span className="hidden md:inline">{route.name}</span>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
