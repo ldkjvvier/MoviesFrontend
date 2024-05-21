03cb8d67d559b9f57851109c54e0b819cf4650a0
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-20 21:13:57 -0400

    feat: Add MoviesCategoryProps interface and Categories object

---
+++ src/modules/movies/utils/Categories.ts
@@ -0,0 +1,20 @@
+
+interface MoviesCategoryProps {
+	Recommended: string
+	Trending: string
+	Popular: string
+	TopRated: string
+	Upcoming: string
+	NowPlaying: string
+}
+
+export const Categories: MoviesCategoryProps = {
+  Recommended: 'Recommended for you',
+  Trending: 'Trending',
+  Popular: 'Popular',
+  TopRated: 'Top Rated',
+  Upcoming: 'Upcoming',
+  NowPlaying: 'Continue Watching',
+}
+
+
