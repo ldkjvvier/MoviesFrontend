ad19393be485d1ec29a154379642c69644d60eff
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-26 04:15:04 -0400

    refactor: Update MoviesCard component to improve UI and performance

--- src/modules/movies/components/MoviesCard.tsx
+++ src/modules/movies/components/MoviesCard.tsx
@@ -1,60 +1,41 @@
-import { movieDuration } from '../utils/MovieDuration'
-import { useFetchMovies } from '../hooks/useFetchMovies'
-import { Loading } from '../../core/components/Loader/Loading'
-export const MoviesCard = () => {
-
-	const { movies, loading, error } = useFetchMovies()
-	if (loading) return <Loading />
-	if (error) return <p>{error}</p>
-
+import { movieDuration } from '../utils/MovieFixDuration.utilities'
+import { Movie } from '../models/Movie'
+export const MoviesCard = ({ movie }: { movie: Movie }) => {
 	return (
-		<div className="px-12">
-			<h1 className="font-bold my-12 mx-3">Movie List</h1>
-			<ul>
-				<li className="grid grid-cols-6 grid-flow-row gap-8">
-					{movies.map((movie) => {
-						return (
-							<div
-								key={movie.id}
-								className="text-lg font-semibold text-center hover:scale-125 transition-all duration-200 ease-in-out"
-							>
-								<div className="group/movie relative">
-									<picture>
-										<img
-											src={movie.poster}
-											alt={movie.title}
-											className="group-hover/movie:opacity-10 h-64 w-full object-cover rounded-lg"
-										/>
-									</picture>
-									<div className="absolute invisible group-hover/movie:visible top-0 w-full h-full flex flex-col p-3 text-start">
-										<h3 className="mb-3 text-base	">{movie.title}</h3>
-										<p className="text-sm">
-											<span>★</span> {movie.rate}
-										</p>
-										<div className="flex gap-3 text-sm font-bold">
-											<span className="border flex px-[12px] py-[0px]">
-												{movie.age}+
-											</span>
-											<p>{movieDuration(movie.duration)}</p>
-										</div>
-										<h4 className="text-sm">{movie.description}</h4>
-										<p>{movie.year}</p>
-										<div className="flex h-full items-end ">
-											<ul className="flex gap-3 text-xs top-12 list-disc list-inside">
-												{movie.genre.map((genre, index) => {
-													return <li key={index}>{genre}</li>
-												})}
-											</ul>
-										</div>
-									</div>
-								</div>
+		<section className="px-3 overflow-visible">
+			<ul className="grid grid-flow-col grid-rows-0 gap-8">
+				<li
+					key={movie.id}
+					className="text-lg font-semibold text-center hover:scale-110 transition-all duration-200 ease-in-out"
+				>
+					<figure className="group/movie relative">
+						<img
+							src={movie.poster}
+							alt={movie.title}
+							className="group-hover/movie:opacity-10 h-44 w-96 object-cover rounded-lg snap-center pointer-events-none"
+						/>
+						<figcaption className="absolute invisible group-hover/movie:visible top-0 w-full h-full flex flex-col p-3 text-start">
+							<h3 className="mb-3 text-base">{movie.title}</h3>
+							<p className="text-sm">
+								<span>★</span> {movie.rate}
+							</p>
+							<div className="flex gap-3 text-sm font-bold">
+								<span className="border flex px-[12px] py-[0px]">
+									{movie.age}+
+								</span>
+								<p>{movieDuration(movie.duration)}</p>
 							</div>
-						)
-					})}
+							<h4 className="text-sm">{movie.description}</h4>
+							<p>{movie.year}</p>
+							<ul className="flex h-full items-end gap-3 text-xs list-disc list-inside">
+								{movie.genre.map((genre, index) => (
+									<li key={index}>{genre}</li>
+								))}
+							</ul>
+						</figcaption>
+					</figure>
 				</li>
 			</ul>
-		</div>
+		</section>
 	)
 }
-
-
