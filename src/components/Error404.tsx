ac4d9ddaeb7a5949d524a5bd3040431e0ca97775
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-26 16:17:41 -0400

    feat: Add Error404 component for handling 404 errors

---
+++ src/components/Error404.tsx
@@ -0,0 +1,8 @@
+export const Error404 = () => {
+/* 404 */
+  return (
+    <>
+      <h1 className="text-white">404</h1>
+    </>
+  )
+}
--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -1,4 +1,5 @@
 import Home from '@/modules/movies/routes/Home'
+import { Error404 } from '@/components/Error404'
 import { createBrowserRouter } from 'react-router-dom'
 
 const routerConfig = [
@@ -7,7 +8,7 @@ const routerConfig = [
 	{ path: '/search', element: <Home /> },
 	{ path: '/movies', element: <Home /> },
 	{ path: '/series', element: <Home /> },
-	{ path: '*', element: <Home /> },
+	{ path: '*', element: <Error404 /> },
 ]
 
 export const router = createBrowserRouter(routerConfig)
