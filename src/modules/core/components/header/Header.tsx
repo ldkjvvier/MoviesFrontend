d99864f21f836aadd53fd37811ec3e9f9c33bbb2
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-26 04:13:16 -0400

    refactor: Update header background color to primary

--- src/modules/core/components/header/Header.tsx
+++ src/modules/core/components/header/Header.tsx
@@ -4,7 +4,7 @@ import { Account } from './Account'
 export const Header = () => {
 	return (
 		<header>
-			<nav className="flex bg-secondary px-3 md:px-12 py-4 justify-between">
+			<nav className="flex bg-primary px-3 md:px-12 py-4 justify-between">
 				<h1 className="text-white text-2xl text-center">
 					Movies
 				</h1>
