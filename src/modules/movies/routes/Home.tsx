12757cc584603e91bc777f859c7815734e9dfbb6
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-28 18:41:08 -0400

    refactor: Update carousel CSS for better styling and responsiveness

--- src/modules/movies/routes/Home.tsx
+++ src/modules/movies/routes/Home.tsx
@@ -1,11 +1,16 @@
 import { Header } from '../../core/components/header/Header'
-import { MoviesCaterogy } from '../components/MoviesByCategory/MoviesCategory'
+import { MoviesCaterogy } from '../components/moviesByCategory/MoviesCategory'
+import { Featured } from '../components/featured/Featured'
 import React from 'react'
+import '../components/carousel.css'
 const Home: React.FC = () => {
 	return (
 		<>
 			<Header />
-			<main className='pt-16 '>
+			<main className="pt-16 px-6 md:px-16 overflow-hidden z-0">
+				<div >
+					<Featured />
+				</div>
 				<MoviesCaterogy />
 			</main>
 		</>
