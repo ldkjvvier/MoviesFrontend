da3f41ef5c91a86bcc80875e0884aa74c0a9e968
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-29 18:12:58 -0400

    refactor: Update Home component to export as named export

--- src/modules/movies/routes/Home.tsx
+++ src/modules/movies/routes/Home.tsx
@@ -2,12 +2,11 @@ import { Header } from '../../core/components/header/Header'
 import { MoviesCaterogy } from '../components/moviesByCategory/MoviesCategory'
 import { Featured } from '../components/featured/Featured'
 import React from 'react'
-import '../components/carousel.css'
-const Home: React.FC = () => {
+export const Home: React.FC = () => {
 	return (
 		<>
 			<Header />
-			<main className="pt-16 px-6 md:px-16 overflow-hidden z-0">
+			<main className="pt-16 px-6 md:px-16 overflow-hidden">
 				<div >
 					<Featured />
 				</div>
@@ -17,4 +16,3 @@ const Home: React.FC = () => {
 	)
 }
 
-export default Home
