ddb6608e1cdd65dc98bcc53323b31ae7b4abcf2a
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-29 18:13:40 -0400

    refactor: Update useFeatured and useMovies hooks to use cache-first fetch policy

--- src/modules/movies/hooks/useFeatured.ts
+++ src/modules/movies/hooks/useFeatured.ts
@@ -2,7 +2,10 @@ import { useQuery } from '@apollo/client'
 import { MoviesData } from '../models/Movie'
 import { GET_FEATURED } from '../graphql/queries/getFeatured'
 export const useFeatured = () => {
-	const { data, loading, error } = useQuery<MoviesData>(GET_FEATURED)
+	const { data, loading, error } = useQuery<MoviesData>(GET_FEATURED,{
+		fetchPolicy: 'cache-first',
+		
+	})
 	// if error redirect to 404 page
 	if (error) window.location.href = '/404'
 	return { movies: data, loading }
--- src/modules/movies/hooks/useMovies.ts
+++ src/modules/movies/hooks/useMovies.ts
@@ -3,7 +3,9 @@ import { MoviesData } from '../models/Movie'
 import { GET_MOVIES } from '../graphql/queries/getMovies'
 
 export const useMovies = () => {
-	const { data, loading, error } = useQuery<MoviesData>(GET_MOVIES)
+	const { data, loading, error } = useQuery<MoviesData>(GET_MOVIES, {
+		fetchPolicy: 'cache-first',	
+	})
 	// if error redirect to 404 page
 	if (error) window.location.href = '/404'
 
