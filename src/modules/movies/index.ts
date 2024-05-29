8b20413a572b78d0aa212bb5c30049907908c4ab
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-05-29 18:15:24 -0400

    feat: Add Movies, Search, and Series routes with corresponding components

---
+++ src/modules/movies/index.ts
@@ -0,0 +1,6 @@
+import './components/carousel.css'
+
+export { Home } from './routes/Home'
+export { Search } from './routes/Search'
+export { Movies } from './routes/Movies'
+export { Series } from './routes/Series'
